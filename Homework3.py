72e4cfba746a5a751d9a73defa616bf1169b6795
Azmt-94 <azmt_is@vk.com>
2024-09-10 23:52:38 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,8 @@
+HW = 12
+Wasted_time = 1.5
+Name = 'Python,'
+Time_HW = (Wasted_time/HW)
+print (Name,'всего задач:',HW,",",'затрачено часов:',
+       Wasted_time,","'среднее время выполнения:',
+       Time_HW,"часа")
+
