6eed5750e17607b7d45bdfcbf89922db4105cae9
Azmt-94 <azmt_is@vk.com>
2024-09-10 23:51:56 +0300

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,20 @@
+# "1st program"
+print (9**0.5*5)
+
+# "2nd program"
+print (999 > 998 and 1000 != 1000.1)
+
+# "3rd program"
+print (2*2+2)
+print (2*2**+2)
+print ((2*2+2) == (2*2**+2))
+
+# "4th program"
+a = str("123.456")
+b = float(123.456*10)
+c = int (1234%10)
+print (a, type (a))
+print (b, type (b))
+print (c, type (c))
+
+
