7e6b5f948a046175ef8c7362b6e3b80a7dc9172a
Azmt-94 <azmt_is@vk.com>
2024-09-11 00:28:07 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Azamat"
+print ("Name:",name, type(name))
+age = 29
+print ("Age:",age, type(age))
+new_age = age + 1
+print ("New age:",new_age, type (new_age))
+is_student = True
+print ("Is student:",is_student, type (is_student))
