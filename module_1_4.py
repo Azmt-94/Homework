1aaa7e64fc284b4b6273b54c700eae34f197ca56
Azmt-94 <azmt_is@vk.com>
2024-09-12 00:08:02 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,19 @@
+#my_string = input("Введите данные: ")
+#print (my_string.upper())
+#print (my_string.lower())
+#print (my_string.replace(" ",""))
+
+
+# Вариант №1 вывести первый символ
+print ("my_string"[0])
+
+# Вариант №2 вывести первый символ
+print ("my_string".replace("my_string","m"))
+
+# Вариант №1 вывести последний символ
+print ("my_string"[-1])
+
+# Вариант №2 вывести последний символ
+print ("my_string".replace("my_string","g"))
+
+
